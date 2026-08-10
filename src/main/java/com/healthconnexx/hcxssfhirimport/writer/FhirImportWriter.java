280843e4883ca8c391036a66bcac3868f53aec6d
Jim Stansell <jim.stansell@rovisity.com>
2026-08-10 07:21:44 -0400

    HDC-229 complete

--- src/main/java/com/healthconnexx/hcxssfhirimport/writer/FhirImportWriter.java
+++ src/main/java/com/healthconnexx/hcxssfhirimport/writer/FhirImportWriter.java
@@ -22,6 +22,7 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * HDC-221: Persists a {@link MappedBundle} to the fhir schema using jOOQ.
